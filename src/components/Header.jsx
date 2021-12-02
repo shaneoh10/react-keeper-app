d91016f20853510da200ccb50b99c62e87981a7d
Shane O'Hanlon <shaneaghada@gmail.com>
2021-12-02 13:37:47 +0000

    Install material-ui components and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HighLightIcon from '@material-ui/icons/Highlight';
 
 function Header() {
     return (
         <header>
-            <h1>Keeper</h1>
+            <h1><HighLightIcon/> Keeper</h1>
         </header>
     );
 }
